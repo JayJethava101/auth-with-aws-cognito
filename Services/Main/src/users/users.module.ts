fd648e961cf310ae599ce5dfbe7d3b3547eb5077
JayJethava101 <jayjethava101@gmail.com>
2025-05-26 10:16:40 +0530

    first commit

---
+++ Services/Main/src/users/users.module.ts
@@ -0,0 +1,17 @@
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { TenantMiddleware } from '../database/tenant.middleware';
+import { TenantModule } from '../tenant/tenant.module';
+@Module({
+  imports: [TenantModule, ],
+  controllers: [UsersController],
+  providers: [UsersService, ],
+})
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(TenantMiddleware)
+      .forRoutes({ path: 'users', method: RequestMethod.ALL });
+  }
+}
